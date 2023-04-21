c519b66259d880f38a96229b9539f36ab0dfffa6
Animesh Goswami <72808045+animeshgos@users.noreply.github.com>
2023-04-21 23:59:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def index():
+    data = request.get_json()
+
+    symptom = data['queryResult']['parameters']['symptoms'][0]
+
+    symptom_id = fetch_symid(symptom)
+
+    diagnosis = fetch_diag(symptom_id)
+    response = {
+        'fulfillmentText': "Diagnosis for {} : \n {}".format(symptom, diagnosis)
+    }
+
+    return jsonify(response)
+
+
+def fetch_symid(name):
+    url = 'https://healthservice.priaid.ch/issues?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFuaW1lc2h6ZW54MTFAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI5NTA4IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMTA5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6IjEwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IkJhc2ljIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAyMy0wNC0xNSIsImlzcyI6Imh0dHBzOi8vYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTY4MjEwODM0NCwibmJmIjoxNjgyMTAxMTQ0fQ.8tuEHvd5mI_qy0UeiHKNITOTGytPjGdYqOU0YVp_1SE&format=json&language=en-gb'
+    response = requests.get(url)
+    response = response.json()
+    response = response[:]
+
+    for dict in response:
+        if dict['Name'] == name:
+            return dict['ID']
+
+
+def fetch_diag(id):
+    url = 'https://healthservice.priaid.ch/issues/{}/info?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFuaW1lc2h6ZW54MTFAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI5NTA4IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMTA5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6IjEwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IkJhc2ljIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAyMy0wNC0xNSIsImlzcyI6Imh0dHBzOi8vYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTY4MjEwODQyOCwibmJmIjoxNjgyMTAxMjI4fQ.OuDvA-YytkKyq3c25w3VtvEMZrs_Z_19xnMJIDvAE2s&format=json&language=en-gb'.format(
+        id)
+    response = requests.get(url)
+    response = response.json()
+    return response['TreatmentDescription']
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
